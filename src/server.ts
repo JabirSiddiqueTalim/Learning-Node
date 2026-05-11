ee5f26d8985fbf9553f243737e441908838ee07c
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-05-12 02:38:57 +0600

    Route Handling with HTTP Requests & Responses

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,35 @@
-import { createServer, IncomingMessage, Server } from "http";
+import { createServer, IncomingMessage, Server, ServerResponse } from "http";
 
-const server:Server=createServer((req:IncomingMessage,res)=>{
-  console.log(req);
+const server:Server=createServer((req:IncomingMessage,res:ServerResponse)=>{
+  // console.log(req.url);
+  // console.log(req.method)
+  const url=req.url;
+  const method=req.method;
+  if(url==='/' && method==='GET')
+  {
+    // console.log("This is url and method");
+    res.writeHead(200,{"content-type":"application/json"})
+ 
+    res.end(JSON.stringify({message:"This is root"}))
+
+  }else if(url?.startsWith("/products"))
+  {
+    res.writeHead(200,{"content-type":"application/json"})
+ 
+    res.end(JSON.stringify({message:"This is products"}))
+
+  }
+  else
+  {
+    // res.writeHead(300,{"content-type":"text"});
+    // res.end("Router is in else option")
+    res.writeHead(200,{"content-type":"application/json"})
+ 
+    res.end(JSON.stringify({message:"This is not root!!!!!!!!!!!"}))
+  }
 
 })
-server.listen(5000,()=>{
+server.listen(1235,()=>{
   console.log("Server is running on post 5000");
 
 })
