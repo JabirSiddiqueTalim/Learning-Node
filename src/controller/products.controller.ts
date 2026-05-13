28a4be99d4ff0b8ccb6ad9b57fa595133bc10424
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-05-13 21:11:35 +0600

    Perfecting PUT Method Implementation

--- src/controller/products.controller.ts
+++ src/controller/products.controller.ts
@@ -1,16 +1,16 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { insertProduct, readProduct } from "../server/product.server";
 
-import type { IProduct} from "../types/product.type"
+import type { IProduct } from "../types/product.type"
 import { parseBody } from "../utility/parseBody";
-export const productHandle = async(req: IncomingMessage, res: ServerResponse) => {
+export const productHandle = async (req: IncomingMessage, res: ServerResponse) => {
   const url = req.url;
   const method = req.method;
   //Get All products
   const reqUrl = url;
   // console.log(reqUrl)
-  const urlPart=reqUrl?.split('/');
-  const id=urlPart && urlPart[1]==='products'?urlPart[2]:null;
+  const urlPart = reqUrl?.split('/');
+  const id = urlPart && urlPart[1] === 'products' ? urlPart[2] : null;
   console.log(id)
   if (url === '/products' && method === 'GET') {
     const product = readProduct();
@@ -21,24 +21,22 @@ export const productHandle = async(req: IncomingMessage, res: ServerResponse) =>
     })
     )
   }
-  else if (method === 'GET' && id!=null)
-  {
+  else if (method === 'GET' && id != null) {
     const products = readProduct();
-    const singleProduct=products.find((p : IProduct) =>p.id === Number(id));
+    const singleProduct = products.find((p: IProduct) => p.id === Number(id));
     // console.log(singleProduct)
     res.writeHead(200, { "content-type": "application/json" })
     res.end(JSON.stringify({
       message: "This is products from controller products.ts",
       data: singleProduct
     })
-    )    
-  }else if(method==="POST" && url==="/products")
-  {
+    )
+  } else if (method === "POST" && url === "/products") {
     const products = readProduct();
-    const body =await parseBody(req);
-    console.log(body);
-    const newProduct={
-      id:Date.now(),
+    const body = await parseBody(req);
+    // console.log(body);
+    const newProduct = {
+      id: Date.now(),
       ...body,
 
     };
@@ -49,11 +47,33 @@ export const productHandle = async(req: IncomingMessage, res: ServerResponse) =>
     res.writeHead(200, { "content-type": "application/json" })
     res.end(JSON.stringify({
       message: "product created",
+      data: products
+    })
+    )
+  } else if (method === 'PUT' && id != null) {
+    const products = readProduct();
+    const body = await parseBody(req);
+    const indexOfPut = products.findIndex((p: IProduct) => p.id === Number(id));
+    // console.log("index",indexOfPut);
+    products[indexOfPut] = { id: products[indexOfPut].id, ...body }
+    console.log(products);
+    if (indexOfPut < 0) {
+      res.writeHead(440, { "content-type": "application/json" })
+      res.end(JSON.stringify({
+        message: "Product not found!!!",
+        data: null
+      })
+      )
+    }else
+    {
+      insertProduct(products);
+      res.writeHead(200, { "content-type": "application/json" })
+      res.end(JSON.stringify({
+      message: "product updated",
       data:products
     })
-    )   
-
-
+    )
+    }
 
   }
 } 
